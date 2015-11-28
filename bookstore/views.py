4c55bbe08aec95ae8a50bce042df5410f1424195
linyijuan <linyijuan22@gmail.com>
2015-11-28 16:45:38 +0800

    abc

--- bookstore/views.py
+++ bookstore/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Book
 import MySQLdb
 
+
+
 def book_list(request):	
 	# Book.objects.create(ISBN = 9780072465631,title ='Database Management Systems',author = 'Raghu Ramakrishnan',publisher ='McGraw-Hill',keywords ='Database',year_of_publication = 2002,price = 47,format ='softcover',copies = 3,subject ='Database');
 	# Book.objects.create(ISBN = 9780132943260,title ='Database Systems: A Practical Approach',author = 'Thomas Connolly',publisher ='Pearson',keywords ='Database',price = 61,year_of_publication = 2014,format ='hardcover',copies = 6,subject ='Database');
@@ -13,11 +15,22 @@ def book_list(request):
 	# Book.objects.create(ISBN = 9780735611313,title ='Code:The Hidden Language of Computer HW and SW',author = 'Charles Petzold',publisher ='Microsoft Press', keywords ='Code',year_of_publication = 2000,price = 65,format ='hardcover',copies = 8,subject ='Programming');
 	# Book.objects.create(ISBN = 9781427798190,title ='Math for Moms and Dads:just for parents',author = 'Kaplan',publisher ='Kaplan Publishing', keywords ='Math',year_of_publication = 2008,price =36,format ='softcover',copies = 5,subject ='Math');
 	# Book.objects.create(ISBN = 9781590282410,title ='Python Programming: An Introduction to CS',author = 'John Zelle',publisher ='Franklin Beedle Inc.', keywords ='Python',year_of_publication = 2010,price =18,format ='softcover',copies = 12,subject ='Programming');
-	return render(request, 'test.html', {'book_list': Book.objects.all()})
+	book_list = ""
+	try:
+		con = mdb.connect(host="localhost",user='root',passwd="",db="bookstore")
+		sql = "select * from %s" %(self.models._meta.bookstore_book)
+		with con:
+			cur = con.cursor()
+			cur.execute(sql)
+			book_list = cur.fetchall()
+	except:
+		book_list = "No Book Available"
+	return render(request, 'test.html', {'book_list': book_list})
 
 def LoginPage(request):
 	return render(request, 'login.html')
 
 def index(request):
 	return render(request, 'index.html',{'book_list': Book.objects.all()})
+
 
